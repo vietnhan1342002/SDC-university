cee727ae8e038938b001dfc3ce39130ecd428674
cudum13402nhan@gmail.com <cudum13402nhan@gmail.com>
2024-07-29 11:09:24 +0700

    fetchNews

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,8 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchAllNewsSlices } from "@/redux/News/newsSlice";
 
 const News = () => {
+    const dispatch = useDispatch();
+    const listNews = useSelector(state => state.news.listNews);
+
+    useEffect(() => {
+        dispatch(fetchAllNewsSlices())
+    }, [dispatch])
     return (
-        <div className="bg-indigo-300">
-            News
+        <div className="flex flex-col m-5 px-52 gap-10">
+
+            {
+                listNews && listNews.length > 0 && listNews.map(item => (
+                    <div key={item.id} className="flex flex-col gap-5">
+                        <div className="flex flex-col gap-5">
+                            <p className="text-2xl font-bold">{item.title}</p>
+                            <p className="text-2xl font-bold">{item.body}</p>
+                            <img src='/images/banner.jpg' alt="" />
+
+                        </div>
+                    </div>
+                ))
+            }
+
+
         </div>
     )
 }
